6e6540c75ebcf003f6b46db90476f95c11beaf07
ian buchholz <ianbuchholz@gmail.com>
2017-08-15 20:24:10 -0700

    function to add list title

--- models/list.js
+++ models/list.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var listSchema = new mongoose.Schema({
   user: String,
   title: String,
-  albums: [{type: Schema.Types.ObjectId, ref: 'Album'}],
+  albums: [{type: mongoose.Schema.Types.ObjectId, ref: 'Album'}],
   created: { type: Date, default: Date.now }
 })
 
